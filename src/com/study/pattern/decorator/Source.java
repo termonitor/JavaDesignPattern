c9888703f2a7b710565e60dedfaf15869b851321
termonitor <pxm2525@163.com>
2017-01-27 10:03:07 +0800

    装饰模式

---
+++ src/com/study/pattern/decorator/Source.java
@@ -0,0 +1,13 @@
+package com.study.pattern.decorator;
+
+
+/**
+ * Created by panxiaoming on 17/1/27.
+ */
+public class Source implements Sourceable {
+
+    @Override
+    public void method() {
+        System.out.println("the original method!");
+    }
+}
