782572d3744d24617c8f0b964337965f99b6fbbf
termonitor <pxm2525@163.com>
2017-01-27 10:53:03 +0800

    组合模式

---
+++ src/com/study/pattern/composite/TreeNode.java
@@ -0,0 +1,45 @@
+package com.study.pattern.composite;
+
+import java.util.Enumeration;
+import java.util.Vector;
+
+/**
+ * Created by panxiaoming on 17/1/27.
+ */
+public class TreeNode {
+    private String name;
+    private TreeNode parent;
+    private Vector<TreeNode> children = new Vector<TreeNode>();
+
+    public TreeNode(String name) {
+        this.name = name;
+    }
+
+    public void add(TreeNode node) {
+        children.add(node);
+    }
+
+    public void remove(TreeNode node) {
+        children.remove(node);
+    }
+
+    public Enumeration<TreeNode> getChildren() {
+        return children.elements();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public TreeNode getParent() {
+        return parent;
+    }
+
+    public void setParent(TreeNode parent) {
+        this.parent = parent;
+    }
+}
