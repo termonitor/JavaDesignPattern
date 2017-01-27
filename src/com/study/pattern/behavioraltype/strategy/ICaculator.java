a437be9354ad5e80d25fd0dea051ff41f707b2c6
termonitor <pxm2525@163.com>
2017-01-27 11:25:23 +0800

    策略模式

---
+++ src/com/study/pattern/behavioraltype/strategy/ICaculator.java
@@ -0,0 +1,8 @@
+package com.study.pattern.behavioraltype.strategy;
+
+/**
+ * Created by panxiaoming on 17/1/27.
+ */
+public interface ICaculator {
+    public int calculate(String exp);
+}
---
+++ src/com/study/pattern/behavioraltype/strategy/Plus.java
@@ -0,0 +1,14 @@
+package com.study.pattern.behavioraltype.strategy;
+
+/**
+ * 策略模式
+ * Created by panxiaoming on 17/1/27.
+ */
+public class Plus extends AbstractCalculator implements ICaculator {
+
+    @Override
+    public int calculate(String exp) {
+        int arrayInt[] = split(exp, "\\+");
+        return arrayInt[0] + arrayInt[1];
+    }
+}
