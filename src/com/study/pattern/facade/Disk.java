1437d1977af434bbec99d04f68fa45b35eb10fcc
termonitor <pxm2525@163.com>
2017-01-27 10:21:54 +0800

    外观模式（Facade）

---
+++ src/com/study/pattern/facade/Disk.java
@@ -0,0 +1,15 @@
+package com.study.pattern.facade;
+
+/**
+ * Created by panxiaoming on 17/1/27.
+ */
+public class Disk {
+
+    public void startup() {
+        System.out.println("disk startup!");
+    }
+
+    public void shutdown() {
+        System.out.println("disk shutdown!");
+    }
+}
---
+++ src/com/study/pattern/facade/Memory.java
@@ -0,0 +1,15 @@
+package com.study.pattern.facade;
+
+/**
+ * Created by panxiaoming on 17/1/27.
+ */
+public class Memory {
+
+    public void startup() {
+        System.out.println("memory startup!");
+    }
+
+    public void shutdown() {
+        System.out.println("memory shutdown!");
+    }
+}
